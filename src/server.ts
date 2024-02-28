a3348fd49bbbda091bba1bae7734fcbfee893340
AmazeDeus <mathias_launonen@yahoo.se>
2024-02-28 22:12:13 +0200

    Listen for Stripe webhook

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import { nextApp, nextHandler } from './next-utils';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './trpc';
 import { inferAsyncReturnType } from '@trpc/server';
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
+import { stripeWebhookHandler } from './webhooks';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -14,12 +17,25 @@ const createContext = ({
 }: trpcExpress.CreateExpressContextOptions) => ({
   req,
   res,
-})
+});
 
 // Infer the context for use in the tRPC initialization - trpc.ts
-export type ExpressContext = inferAsyncReturnType<typeof createContext>
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
+
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer;
+};
 
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buf) => {
+      req.rawBody = buf;
+    },
+  });
+
+  // Stripe webhook (By validating the expected signature, we can make sure the req comes from Stripe and not just any req calling the webhook)
+  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler);
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
@@ -42,10 +58,12 @@ const start = async () => {
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    payload.logger.info("Next.js started")
+    payload.logger.info('Next.js started');
 
     app.listen(PORT, async () => {
-      payload.logger.info(`Next.js App Url: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+      payload.logger.info(
+        `Next.js App Url: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
     });
   });
 };
