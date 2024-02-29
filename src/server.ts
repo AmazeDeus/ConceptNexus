86d61b1033f77e369b64e78c2c90d8d0bf03ad44
AmazeDeus <mathias_launonen@yahoo.se>
2024-02-29 17:55:35 +0200

    Deployment configuration

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { inferAsyncReturnType } from '@trpc/server';
 import bodyParser from 'body-parser';
 import { IncomingMessage } from 'http';
 import { stripeWebhookHandler } from './webhooks';
+import nextBuild from 'next/dist/build';
+import path from 'path';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -44,6 +46,20 @@ const start = async () => {
       },
     },
   });
+
+  // Building Next.js
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info('Next.js is building for production');
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'));
+
+      process.exit();
+    });
+
+    return;
+  }
 
   // Forward any request to this endpoint to trpc in next.js endpoints
   app.use(
