3f696da477f94d5d2b1e356bdc7f94e2eac2d349
AmazeDeus <mathias_launonen@yahoo.se>
2024-05-05 18:42:47 +0300

    update hostname

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'conceptnexus-production.up.railway.app',
+        hostname: 'concept-nexus.vercel.app',
       },
     ],
   },
